52376342df3b356c1f7a4694575d7a9a01c92352
unknown <crazeddemonanarch@gmail.com>
2021-03-22 16:14:10 +0000

    Refactored code in about section

--- src/components/homepage/About.vue
+++ src/components/homepage/About.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="about" ref="about">
     <div class="intro">
-      <transition :css="false" @enter="enter($event, 300)">
-        <h3
-          v-show="animate"
-          class="about__section-title about__section-title--animate"
-        >
-          About
-        </h3>
-      </transition>
-      <div class="intro__title-wrapper">
+      <div class="headings">
+        <transition :css="false" @enter="enter($event, 300)">
+          <h3 v-show="animate" class="headings__section-title">
+            About
+          </h3>
+        </transition>
         <transition :css="false" @enter="enter($event, -300)">
-          <h1 v-show="animate" class="title intro__title">
+          <h1 v-show="animate" class="titles headings__title">
             From thought to reality
           </h1>
         </transition>
         <transition :css="false" @enter="enter($event, 300)">
-          <div v-show="animate" class="underline"></div>
+          <div v-show="animate" class="headings__underline"></div>
         </transition>
       </div>
       <div class="intro__paragraphs">
@@ -37,26 +34,42 @@
       </div>
     </div>
     <div class="cards">
-      <div class="cards__card cards__card--1"><h1>Hello</h1></div>
-      <div class="cards__card cards__card--2"><h1>Greetings</h1></div>
-      <div class="cards__card cards__card--3"><h1>Hi</h1></div>
+      <transition :css="false" @enter="enter($event, -300)">
+        <div v-show="animate" class="cards__card cards__card--1">
+          <h1>Hello</h1>
+        </div>
+      </transition>
+
+      <transition :css="false" @enter="enter($event, -300, 'Y')">
+        <div v-show="animate" class="cards__card cards__card--2">
+          <h1>Greetings</h1>
+        </div>
+      </transition>
+
+      <transition :css="false" @enter="enter($event, 300)">
+        <div v-show="animate" class="cards__card cards__card--3">
+          <h1>Hi</h1>
+        </div>
+      </transition>
     </div>
     <div class="outro">
       <div class="outro-text outro-text--1">
-        <transition :css="false" @enter="enter($event, 300)">
-          <div v-show="animate" class="outro-text__label">Advantages</div>
-        </transition>
-        <div class="intro__title-wrapper">
+        <div class="headings">
+          <transition :css="false" @enter="enter($event, 300)">
+            <h3 v-show="animate" class="headings__section-title">
+              Advantages
+            </h3>
+          </transition>
           <transition :css="false" @enter="enter($event, -300)">
-            <h1 v-show="animate" class="title outro-text__title">
+            <h1 v-show="animate" class="headings__title">
               Working on exclusive projects
             </h1>
           </transition>
           <transition :css="false" @enter="enter($event, 300)">
-            <div v-show="animate" class="underline"></div>
+            <div v-show="animate" class="headings__underline"></div>
           </transition>
         </div>
-        <p class="outro-text__paragraph">
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, vel
           ipsum ea dolor cum sint consectetur rerum ipsa voluptas excepturi,
           odit magni! Labore quidem vero, aut totam, at repudiandae dignissimos
@@ -65,9 +78,11 @@
         <a href="#" class="button button--outro-text">View More</a>
       </div>
       <div class="outro-text outro-text--2">
-        <div class="outro-text__label">Advantages</div>
-        <h1 class="title outro-text__title">Working on exclusive projects</h1>
-        <p class="outro-text__paragraph">
+        <div class="headings">
+          <h3 class="headings__section-title">Advantages</h3>
+          <h1 class="headings__title">Working on exclusive projects</h1>
+        </div>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, vel
           ipsum ea dolor cum sint consectetur rerum ipsa voluptas excepturi,
           odit magni! Labore quidem vero, aut totam, at repudiandae dignissimos
@@ -86,7 +101,7 @@ export default {
   },
   methods: {
     // Transition function, takes event data and the inital distance which can be positive or negative
-    enter(e, initialValue) {
+    enter(e, initialValue, axis = "X") {
       // Assigns the number sign and makes initalValue an absolute value
       let numberSign = "+";
       if (initialValue < 0) {
@@ -110,7 +125,7 @@ export default {
           initialValue -
           Math.min((initialValue / 100) * timeElapsed, initialValue);
         // Transforms the element
-        e.style.transform = `translate(${numberSign}${calculatedNewValue}px)`;
+        e.style.transform = `translate${axis}(${numberSign}${calculatedNewValue}px)`;
         // BASE CASE - is 100 because we divide initialValue by 100. Once time is equal to 100 initialValue subtracts 100% of itself meaning transform translate is now 0 and element is in final position
         if (timeElapsed < 100) {
           requestAnimationFrame(animateSlideIn);
@@ -146,63 +161,160 @@ export default {
 
 <style lang="scss" scoped>
 .about {
-  position: relative;
-  padding-top: $section-padding-top;
-  width: 100%;
-  min-height: 100vh;
-  display: grid;
-  grid:
-    repeat(auto-fill, minmax(0, min-content)) / minmax(0, 1fr)
-    minmax(auto, 1000px)
-    minmax(0, 1fr);
-
-  &__section-title {
-    position: absolute;
-    grid-column: 2 / 3;
-    top: 7rem;
-    margin-bottom: 3rem;
+  padding: 1rem 5rem;
+  @media screen and (min-width: 700px) {
+    position: relative;
+    padding-top: $section-padding-top;
+    width: 100%;
+    min-height: 100vh;
+    display: grid;
+    grid:
+      repeat(auto-fill, minmax(0, min-content)) / minmax(0, 1fr)
+      minmax(auto, 1000px)
+      minmax(0, 1fr);
   }
 }
 
 .intro {
-  grid-column: 2 / 3;
-  display: flex;
-  justify-content: space-between;
+  width: 100%;
+  @media screen and (min-width: 700px) {
+    grid-column: 2 / 3;
+    display: flex;
+    flex-wrap: wrap;
 
-  &__title-wrapper {
-    position: relative;
-    height: fit-content;
-    width: 40%;
-  }
+    justify-content: space-between;
 
-  &__title {
-    position: relative;
-    width: 100%;
-    height: fit-content;
-    font-size: $font-size-header--lg;
-    text-transform: uppercase;
-    padding-right: 7rem;
-  }
-
-  &__paragraphs {
-    width: calc(60%);
-
-    & > p:first-child {
-      font-weight: 600;
-      margin-bottom: 4rem;
+    & .headings {
+      width: 40%;
     }
+
+    &__paragraphs {
+      width: calc(60%);
+
+      & > p:first-child {
+        font-weight: 600;
+        margin-bottom: 4rem;
+      }
+    }
+  }
+}
+
+.outro {
+  color: $color-white;
+  position: relative;
+  width: 100%;
+  margin-top: 33rem;
+  grid-column: 1 / 4;
+  display: flex;
+  flex-wrap: wrap-reverse;
+
+  // @media screen and (min-width: 500px) {
+  //   align-items: center;
+  //   justify-content: center;
+  // }
+  & .outro-text {
+    & > p {
+      margin-top: 3rem;
+      margin-bottom: 3rem;
+    }
+
+    &--1 {
+      @media screen and (min-width: 1200px) {
+        position: relative;
+        height: 89rem;
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        padding-bottom: 15rem;
+        width: calc(calc(1328 / #{$body-width}) * 100%);
+        align-items: flex-start;
+        justify-content: flex-end;
+
+        & > * {
+          margin-left: 21.686%;
+          color: $color-white;
+          width: 36.5%;
+        }
+      }
+      @media screen and (min-width: 600px) {
+        color: $color-white;
+        padding: 5rem;
+        background: $color-black;
+
+        & > p {
+          color: $color-white;
+        }
+      }
+    }
+
+    & > * {
+    }
+
+    &--2 {
+      @media screen and (min-width: 1200px) {
+        right: 0rem;
+        color: $color-black;
+        width: 118rem;
+        max-height: 128rem;
+        z-index: 2;
+        -webkit-font-smoothing: antialiased;
+        display: flex;
+        flex-direction: column;
+        height: 60vw;
+        padding-bottom: 22rem;
+        width: calc(calc(944 / #{$body-width}) * 100%);
+        align-items: flex-end;
+        justify-content: flex-end;
+        position: absolute;
+        bottom: 23rem;
+
+        & > * {
+          width: 67%;
+          margin-right: calc(33% - 14rem);
+        }
+      }
+      @media screen and (min-width: 600px) {
+        padding: 5rem;
+        color: $color-black;
+        background: center / 170% no-repeat
+          url("https://cdn.pixabay.com/photo/2018/11/22/23/57/london-3833039_960_720.jpg")
+          rgba(255, 145, 0, 0.644);
+        background-blend-mode: screen;
+      }
+    }
+  }
+}
+
+.button--outro-text {
+  background: none;
+  border: 2px solid $color-primary;
+  color: $color-primary;
+
+  &:hover {
+    background: $color-primary;
+    color: $color-white;
   }
 }
 
 .cards {
   --card-width: 349px;
-  width: calc(349px * 3 + 90px);
-  place-self: center;
+  --card-spacing: 6rem;
   grid-column: 1 / 4;
+  place-self: center;
   margin-top: 13rem;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
   z-index: 3;
+  // &:nth-child(even) {
+  //   margin-left: 6rem;
+  //   margin-right: 6rem;
+  // }
+  @media screen and (min-width: 1150px) {
+    width: calc(var(--card-width) * 3 + calc(var(--card-spacing) * 2));
+    flex-direction: row;
+  }
 
   &__card {
     width: 349px;
@@ -211,6 +323,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: flex-end;
+    margin: 3rem 0;
     & > h1 {
       color: $color-white;
       margin-bottom: 10rem;
@@ -236,129 +349,5 @@ export default {
         #000;
     }
   }
-}
-
-.outro {
-  color: $color-white;
-  position: relative;
-  width: 100%;
-  margin-top: 33rem;
-  grid-column: 1 / 4;
-
-  & .outro-text {
-    &--1 {
-      position: relative;
-      background: $color-black;
-      width: 166rem;
-      height: 89rem;
-      z-index: 1;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      justify-content: flex-end;
-      color: $color-white;
-
-      & > * {
-        width: 36.5%;
-        margin-left: 36rem;
-      }
-
-      & :last-child {
-        margin-bottom: 15rem;
-      }
-    }
-    &--2 {
-      position: absolute;
-      right: 0rem;
-      bottom: 23rem;
-      color: $color-black;
-      width: 118rem;
-      height: 128rem;
-      z-index: 2;
-      -webkit-font-smoothing: antialiased;
-
-      background: center / 170% no-repeat
-        url("https://cdn.pixabay.com/photo/2018/11/22/23/57/london-3833039_960_720.jpg")
-        rgba(255, 145, 0, 0.644);
-      background-blend-mode: screen;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-
-      & * {
-        width: 67%;
-        margin-left: 14rem;
-      }
-      & > :last-child {
-        margin-bottom: 22rem;
-      }
-    }
-
-    &__label {
-      font-size: $font-size-header--sm;
-      margin-bottom: 2rem;
-    }
-
-    &__title {
-      position: relative;
-      font-size: $font-size-header--md;
-      font-weight: bold;
-      text-transform: uppercase;
-      margin-bottom: 8rem;
-    }
-
-    &__paragraph {
-      margin-bottom: 2rem;
-      color: inherit;
-    }
-  }
-}
-
-.button--outro-text {
-  background: none;
-  border: 2px solid $color-primary;
-  color: $color-primary;
-
-  &:hover {
-    background: $color-primary;
-    color: $color-white;
-  }
-}
-
-.slide-right-title-enter-from {
-  opacity: 0;
-  transform: translateX(-30rem);
-}
-.slide-right-title-enter-active {
-  transition: all 1s ease-in;
-}
-.slide-right-title-enter-to {
-  opacity: 1;
-  transform: none;
-}
-
-.slide-left-section-title-enter-from {
-  opacity: 0;
-  transform: translateX(30rem);
-}
-.slide-left-section-title-enter-active {
-  transition: all 1s ease-in;
-}
-.slide-left-section-title-enter-to {
-  opacity: 1;
-  transform: none;
-}
-
-.slide-left-underline-enter-from {
-  opacity: 0;
-  transform: translateX(30rem);
-}
-.slide-left-underline-enter-active {
-  transition: all 1s ease-in;
-}
-.slide-left-underline-enter-to {
-  opacity: 1;
-  transform: none;
 }
 </style>
