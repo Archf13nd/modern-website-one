16399af98ad5bf157313c2c2029ccb2a47b94180
unknown <ideaswrittenonleaves@gmail.com>
2023-07-28 15:49:43 +0100

    Updated router history mode

--- src/router/routes.js
+++ src/router/routes.js
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = new createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   // scrollBehavior(to, from, savedPosition) {
   //   if (savedPosition) {
